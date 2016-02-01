6732038ebdce1373d471c887e60fbbdaad6ccd29
buddybu <listbuddy@frontier.com>
2016-02-01 12:16:31 -0800

    cleaned up whitespace

--- src/org/usfirst/frc/team2903/robot/RobotMap.java
+++ src/org/usfirst/frc/team2903/robot/RobotMap.java
@@ -8,25 +8,25 @@ package org.usfirst.frc.team2903.robot;
 public class RobotMap {
 
 	
-	//drive
+    //drive
     public static int FrontLeftdrive = 3;
     public static int BackLeftdrive = 4;
     public static int FrontRightdrive = 2;
     public static int BackRightdrive = 5;
 
-	//Gyro
-	public static int Gyro = 0;
+    //Gyro
+    public static int Gyro = 0;
 
-	//Shooters
-	public static int ShooterMotor1 = 0;
-	public static int ShooterMotor2 = 1;
+    //Shooters
+    public static int ShooterMotor1 = 0;
+    public static int ShooterMotor2 = 1;
 	
-	//Lift
-	public static int LiftMotor = 2;
+    //Lift
+    public static int LiftMotor = 2;
 	
-	//Winch
-	public static int WinchMotor = 3;
+    //Winch
+    public static int WinchMotor = 3;
 	
-	public static int KickingMotor = 4;
+    public static int KickingMotor = 4;
 	
 }
