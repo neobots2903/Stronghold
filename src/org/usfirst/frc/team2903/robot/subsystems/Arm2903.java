a97efc8d53930a561cec548dfafbe2b7853c2771
groot314 <groot314@gmail.com>
2016-02-17 18:17:20 -0800

    fixed uncommented and created arm movements with potentiometer

--- src/org/usfirst/frc/team2903/robot/subsystems/Arm2903.java
+++ src/org/usfirst/frc/team2903/robot/subsystems/Arm2903.java
@@ -2,68 +2,72 @@ package org.usfirst.frc.team2903.robot.subsystems;
 
 import org.usfirst.frc.team2903.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.CANTalon;
 //import edu.wpi.first.wpilibj.
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 public class Arm2903 extends Subsystem {
 
-
 	public static CANTalon armMotor;
 	boolean isReset;
-	//Potention
-	
+	// Potention
+
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-public Arm2903 () {
+	Potentiometer pot;
 
-	armMotor = new CANTalon(RobotMap.armMotor);
-	
-	
-}
-	
-//	public void resetArm(){
-//		
-//		if potentiometer > 0 {
-//
-//			armMotor.set(-0.1);
-//			isReset = false; 
-//		
-//		}
-//		
-//		if potentiometer < 0 {
-//		
-//			armMotor.set(0.1);
-//			isReset = false;
-//		
-//		}
-//		
-//		else {
-//		
-//			armMotor.set(0);
-//			isReset = true;
-//		
-//		}
-//		
-//	}
-	
-	public void raiseArm(){
-		
-		armMotor.set(0.1);
-		
+	public Arm2903() {
+
+		armMotor = new CANTalon(RobotMap.armMotor);
+
+		pot = new AnalogPotentiometer(1, 100, 0);
+
 	}
-	
-//	public void setTargetPos(value){
-//		
-//		TargetPos = value;
-//		
-//	}
-	
+
+	public void resetArm() {
+
+		if (pot.get() > 0) {
+
+			armMotor.set(-0.1);
+			isReset = false;
+
+		}
+
+		else {
+
+			armMotor.set(0);
+			isReset = true;
+
+		}
+
+	}
+
+	public void raiseArm() {
+
+		if (pot.get() < 100)
+			armMotor.set(0.1);
+		else
+			armMotor.set(0);
+	}
+
+	public void lowerArm() {
+		if (pot.get() > 0)
+			armMotor.set(-0.1);
+		else
+			armMotor.set(0)
+	}
+
+	// public void setTargetPos(value){
+	//
+	// TargetPos = value;
+	//
+	// }
 
 }
-
-
